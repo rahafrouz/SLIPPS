37da340586036a55df2e7cdbd491f9971eea4b1c
An Pham <ptan1991@gmail.com>
2018-04-04 17:47:25 +0300

    no message

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,12 +1,31 @@
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
 import React, { Component } from "react";
 import Navigation from "../components//Navigation";
 import AdvancedSearchResults from "../components/AdvancedSearchResults";
 import Footer from "../components/Footer";
-import SearchApi from "../apis/SearchApi";
+import request from "superagent";
+
+// import SearchApi from "../apis/SearchApi";
 
 
 var testresults = {"took":8,"timed_out":false,"_shards":{"total":5,"successful":5,"skipped":0,"failed":0},"hits":{"total":1,"max_score":1.2993155,"hits":[{"_index":"slipps","_type":"event","_id":"2","_score":1.2993155,"_source":{"why_relevant":"ANSWER_B_WHY_RELEVANT 2","country":{"name":"Finland","code":"FI"},"@timestamp":"2018-04-02T18:54:16.939Z","keywords":[{"kw_pkey":46,"language_code":"fi","category":"basic","kw_id":3,"content":"Varmistus"},{"kw_pkey":48,"language_code":"fi","category":"basic","kw_id":5,"content":"Ruoka ja ravitsemus"},{"kw_pkey":50,"language_code":"fi","category":"basic","kw_id":7,"content":"Johtajuus"},{"kw_pkey":53,"language_code":"fi","category":"basic","kw_id":10,"content":"Informaation siirto"},{"kw_pkey":74,"language_code":"fi","category":"basic","kw_id":31,"content":"Mielenterveyden/Psykiatrinen osasto/yksikkö"}],"@version":"1","description":"ANSWER_A_EVENT_DESCRIPTION 2","language":{"name":"Finnish","code":"fi"},"id":2,"short_desc":"2 - This is a short version of description to display...","tags":["_aggregatefinalflush"]}}]}};
+// var testresults = {
+//   "hits": {
+//     "total": 0
+//   }
+// };
 class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    request
+      .get("http://127.0.0.1:8000/api/search")
+      .query({ kw: props.match.params.keyword }) // sends a JSON post body
+      .set("content-type", "json")
+      .end((err, res) => {
+        testresults = res.body;
+        this.render();
+      });
+  }
   render() {
     return (
       <div>
@@ -22,9 +41,19 @@ class SearchPage extends Component {
   // since we have `shouldCallAPI` in place
   // we don't need to worry about making pointless requests to the server 
   // if we have more than one component on the page 
-  componentDidMount() {
-    SearchApi.searchByKeyword(this.props.match.params.keyword);
-  }
+  // componentDidMount() {
+  //   request
+  //     .get("http://127.0.0.1:8000/api/search")
+  //     .query({ kw: this.props.match.params.keyword }) // sends a JSON post body
+  //     // .set("Access-Control-Allow-Origin", "Yes")
+  //     .set("content-type", "json")
+  //     .end((err, res) => {
+  //       testresults.results = res.body;
+  //       // console.warn(res);
+  //     // Calling the end function will send the request
+  //     });
+  //   // SearchApi.searchByKeyword(this.props.match.params.keyword);
+  // }
 }
 
 export default SearchPage;
