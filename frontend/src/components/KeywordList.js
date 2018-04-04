8a6310fea404c233182854f21230c1b04942c356
AmiR <rahafrouz@gmail.com>
2018-04-05 00:55:52 +0300

    fixed error when there is no result for search

--- frontend/src/components/KeywordList.js
+++ frontend/src/components/KeywordList.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 class KeywordList extends Component {
-  $resultvar={};
-  constructor(props){
-    super(props);
-    if(!(Object.keys(this.props.Keywords).length === 0))
-    {
-      this.$resultvar=this.props.Keywords.map((kw)=>{return (<li><a href={"/search/"+kw.content}>{kw.content}</a><hr></hr></li>);});
-    }
-  }
+
   render() {
-    return (	
-      <div className="box_general_3">					
-        <ul >
-          {!(Object.keys(this.props.Keywords).length === 0)?this.props.Keywords.map((kw)=>{return (<li><a href={"/search/"+kw.content}>{kw.content}</a><hr></hr></li>);}):"nothing"}
-        </ul>
-      </div>		
-    );
+    if(this.props.Keywords)
+    {
+      return (  
+        <div className="box_general_3">         
+          <ul >
+            {!(Object.keys(this.props.Keywords).length === 0)?this.props.Keywords.map((kw)=>{return (<li><a href={"/search/"+kw.content}>{kw.content}</a><hr></hr></li>);}):"nothing"}
+          </ul>
+        </div>    
+      );
+    }else
+    {
+      return "nothing";
+    }
+
   }
 }
 export default KeywordList;
