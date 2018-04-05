ed39fe7a0da816b6ae06d86af1e971162aa4495e
An Pham <ptan1991@gmail.com>
2018-04-05 11:22:47 +0300

    - Improved keyword list showing no result.

--- frontend/src/components/KeywordList.js
+++ frontend/src/components/KeywordList.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 class KeywordList extends Component {
 
   render() {
-    if(this.props.Keywords)
-    {
+    if (this.props.Keywords) {
       return (  
         <div className="box_general_3">         
-          <ul >
-            {!(Object.keys(this.props.Keywords).length === 0)?this.props.Keywords.map((kw)=>{return (<li><a href={"/search/"+kw.content}>{kw.content}</a><hr></hr></li>);}):"nothing"}
+          <ul>
+            {
+              !(Object.keys(this.props.Keywords).length === 0) ? 
+                this.props.Keywords.map((kw)=>{return (<li><a href={"/search/"+kw.content}>{kw.content}</a><hr></hr></li>);}) :
+                (<span className="message">No keyword to show</span>)
+            }
           </ul>
         </div>    
       );
-    }else
-    {
-      return "nothing";
+    } else {
+      return (
+        <span className="message">No keyword to show</span>
+      );
     }
 
   }
