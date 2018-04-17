0bcb02b62974942ad790ae63291c3f16c37a9285
AmiR <rahafrouz@gmail.com>
2018-04-18 01:34:53 +0300

    fixed css build

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -208,7 +208,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/, /\.css$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -222,7 +222,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/,/\.json$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
