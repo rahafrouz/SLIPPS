3c599a2ec3440c06d90aabd6f0fa4cd14fc16984
An Pham <ptan1991@gmail.com>
2018-04-17 23:27:09 +0300

    - Use server

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -15,8 +15,8 @@ const tokenPlugin = req => {
     req.set("Authorization", `JWT ${token}`);
   }
 };
-// const API_ROOT = "http://slipps.it.lut.fi:8000/api";
-const API_ROOT = "http://localhost:8000/api";
+const API_ROOT = "http://slipps.it.lut.fi:8000/api";
+// const API_ROOT = "http://localhost:8000/api";
 const responseBody = res => res.body;
 
 const requests = {
