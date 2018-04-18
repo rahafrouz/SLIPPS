6db81d76e9fc8dffa0dbc3c24aaddc23589e5db4
An Pham <ptan1991@gmail.com>
2018-04-18 13:26:52 +0300

    upload access fix

--- frontend/src/pages/DocumentUpload.js
+++ frontend/src/pages/DocumentUpload.js
@@ -142,87 +142,106 @@ class DocumentUpload extends Component {
 
 
   render(){
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="upload-page container margin_60">
-          <div className="row">
-            <div className="col-xl-8 col-lg-8">
-              <div className="box_general_3 cart">
-                <div className="form_title">
-                  <h3><strong>1</strong>Upload The Document</h3>
-                  <p>
+    if (this.props.currentUser) {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div className="upload-page container margin_60">
+            <div className="row">
+              <div className="col-xl-8 col-lg-8">
+                <div className="box_general_3 cart">
+                  <div className="form_title">
+                    <h3><strong>1</strong>Upload The Document</h3>
+                    <p>
                   Upload the document generated from LERT tool.
-                  </p>
-                </div>
-                <div className="step first-step">
-                  <div className="box_general_3 ">          
-                    <div className="indent_title_in">
-                      <h3>Drag file here</h3>
-                    </div>
-                    <div className="wrapper_indent form-group">
-                      <input type="file" className="form-control" onChange={this.handleFileChange}
-                        ref={ input => { this.fileInput = input; }}
-                      />
-                    </div>
-                  </div>  
-                </div>
-                <hr />
-                {/*End step */}
-                <div className="form_title">
-                  <h3><strong>2</strong>Add Description</h3>
-                  <p>
+                    </p>
+                  </div>
+                  <div className="step first-step">
+                    <div className="box_general_3 ">          
+                      <div className="indent_title_in">
+                        <h3>Drag file here</h3>
+                      </div>
+                      <div className="wrapper_indent form-group">
+                        <input type="file" className="form-control" onChange={this.handleFileChange}
+                          ref={ input => { this.fileInput = input; }}
+                        />
+                      </div>
+                    </div>  
+                  </div>
+                  <hr />
+                  {/*End step */}
+                  <div className="form_title">
+                    <h3><strong>2</strong>Add Description</h3>
+                    <p>
                   Add description regarding the file you are uploading.
-                  </p>
+                    </p>
+                  </div>
+                  <div className="step second-step">
+                    <textarea className="document_description"
+                      value={this.state.description}
+                      onChange={this.handleInputChanged} name="description">
+                    </textarea>
+                  </div>
+                  <hr />
+                  {/*End step */}
+
+
+
                 </div>
-                <div className="step second-step">
-                  <textarea className="document_description"
-                    value={this.state.description}
-                    onChange={this.handleInputChanged} name="description">
-                  </textarea>
+              </div>
+              {/* /col */}
+              <aside className="col-xl-4 col-lg-4" id="sidebar">
+                <div className="box_general_3 booking">
+                  <form>
+                    <div className="form_title">
+                      <h3><strong>3</strong>Upload!</h3>
+                      <p>Careful!</p>
+                    </div>
+                    <ul className="treatments checkout clearfix">
+                      <li>
+                    Your document should be anonymized using LERT tool.
+                      </li>
+                      <li>
+                      You are responsible for the provided data.
+                      </li>
+                      <li >
+                    Your document would be approved by admin and you will be notified.
+                      </li>
+                    </ul>
+                    <hr />
+                    <label>
+                      <input type="checkbox" style={{paddingRight: "10px"}}
+                        checked={this.state.termsChecked} name="termsChecked" onChange={this.handleInputChanged} />
+                      <span> I read and agree with the <strong>Terms of use</strong></span>
+                    </label>
+                    <button className="btn_1 width_20 medium">Accept and Upload</button>
+                  </form>
                 </div>
-                <hr />
-                {/*End step */}
-
-
-
+                {/* /box_general */}
+              </aside>
+              {/* /asdide */}
+            </div>
+            {/* /row */}
+          </div>
+        </form>
+      );
+    } else {
+      return (
+        <div id="error_page">
+          <div class="container">
+            <div class="row justify-content-center text-center">
+              <div class="col-xl-7 col-lg-9">
+                <h2>:-( <i class="icon_error-triangle_alt"></i></h2>
+                <p>We're sorry, but you need to login to see this page</p>
+                <p>
+                  <Link to="/signup" className="nav-link">Sign Up for a new account</Link>
+                  Or <Link to="/login" className="nav-link">Log In</Link>
+                </p>
               </div>
             </div>
-            {/* /col */}
-            <aside className="col-xl-4 col-lg-4" id="sidebar">
-              <div className="box_general_3 booking">
-                <form>
-                  <div className="form_title">
-                    <h3><strong>3</strong>Upload!</h3>
-                    <p>Careful!</p>
-                  </div>
-                  <ul className="treatments checkout clearfix">
-                    <li>
-                    Your document should be anonymized using LERT tool.
-                    </li>
-                    <li>
-                      You are responsible for the provided data.
-                    </li>
-                    <li >
-                    Your document would be approved by admin and you will be notified.
-                    </li>
-                  </ul>
-                  <hr />
-                  <label>
-                    <input type="checkbox" style={{paddingRight: "10px"}}
-                      checked={this.state.termsChecked} name="termsChecked" onChange={this.handleInputChanged} />
-                    <span> I read and agree with the <strong>Terms of use</strong></span>
-                  </label>
-                  <button className="btn_1 width_20 medium">Accept and Upload</button>
-                </form>
-              </div>
-              {/* /box_general */}
-            </aside>
-            {/* /asdide */}
           </div>
-          {/* /row */}
         </div>
-      </form>
-    );
+      );
+    }
   }
 
 
